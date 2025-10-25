666c70d695f0d799405c526829647df91d481de1
Noname-29-lnin <128560496+Noname-29-lnin@users.noreply.github.com>
2025-10-25 09:47:51 +0700

    update 04_imple

---
+++ 04_imple/Yosys_synthis/Question6/flist.f
@@ -0,0 +1,7 @@
+/home/noname/Documents/project_tiny/Ex3/02_rtl/Question6/Bitonic_Sort.sv
+/home/noname/Documents/project_tiny/Ex3/02_rtl/Question6/Bitonic_Block4.sv
+/home/noname/Documents/project_tiny/Ex3/02_rtl/Question6/Bitonic_Block8.sv
+/home/noname/Documents/project_tiny/Ex3/02_rtl/Question6/COMP_2bit.sv
+/home/noname/Documents/project_tiny/Ex3/02_rtl/Question6/COMP_4bit.sv
+/home/noname/Documents/project_tiny/Ex3/02_rtl/Question6/COMP_less.sv
+/home/noname/Documents/project_tiny/Ex3/02_rtl/Question6/Compare_and_Swap_unit.sv
